15efe94d98bad70f38a906fa4101dad5c0421c79
Ankit-Pruthi <ankitpruthi11@gmail.com>
2025-09-27 19:00:58 +0530

    Renamed folder

---
+++ food-recipe-chrome-extension_v1/config.js
@@ -0,0 +1,5 @@
+// chrome-extension/config.example.js
+export const CONFIG = {
+  LAMBDA_API_URL: "https://fk1pn6sup6.execute-api.us-east-1.amazonaws.com/Prod/hfproxy",
+  LAMBDA_API_KEY: "EpLUU8oWEe2E6YHDMagJ76NwRi0nUWT72mZC4vXu"
+};
